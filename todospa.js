ac82397ad844136a519aed31591ceff53bfef326
browem03 <browem03@luther.edu>
2015-10-16 14:16:06 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,5 +1,11 @@
 
 
-<input type="text" value="To Do" size="40"> <button>New</button>
+changeWord=function(){
+      var myh1 
+      value=document.querySelector("#def").value;
+    
+       myh1=document.querySelector("#abc") 
+       myh1.innerHTML= value;
+       myh1.className="csclass";}
 
 
