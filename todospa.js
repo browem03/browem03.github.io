0bd56b4a578f8be50ebdc7b06c5cb5f5da3cacd6
browem03 <browem03@luther.edu>
2015-10-21 14:13:06 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,11 +1,24 @@
 
 
 changeFunc=function(){
-      var myp
-      value=document.querySelector("#abc").value;
-    
-       myp=document.querySelector("#def") 
-       myp.innerHTML= value;
-       myp.className="csclass";}
-
+      changeFunc=function(){
+      var myh1, high, med, low, myInput, myLi, myol
+       myInput=document.querySelector("#def").value;
+       myh1=document.querySelector("myLi"); 
+       myh1.innerHTML= myInput;
+       
+       
+       myLi = document.createElement(“li”);
+       mylit = document.createTextNode(value);
+       myLi.appendChild(mylit);
+       document.body.appendChild(myLi);
+       myol= document.body.querySelector("#todoList");
+       myol.appendChild(myLi);
+       myLi.className=mypriority.value;
+       myLi.type="checkbox"
+       mypriority=document.querySelector= #priority.value
+        
+        
+        
+    }
 
