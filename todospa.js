f8909d07d09c75a58b2094239a2ad2d284a0d19d
browem03 <browem03@luther.edu>
2015-10-22 22:36:02 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -23,6 +23,5 @@ changeFunc = function(){
       checkfunc = function() { if (this.checked) {this.parentNode.classList.add("done"); }
       else { this.parentNode.classList.remove("done") }
       localSave("todoList");
-      restoreList("todoList", "checkfunc");
       }
   
